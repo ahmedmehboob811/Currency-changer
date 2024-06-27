ed9f7aedd946b877f48f9c352faf6df9dd0aecdf
ahmedmehboob811 <160405430+ahmedmehboob811@users.noreply.github.com>
2024-06-27 08:34:12 -0700

    first commit

---
+++ main.ts
@@ -0,0 +1,52 @@
+#! bin/usr/evn/mode
+import inquirer from "inquirer";
+ 
+    const currency:any = {
+         USD :1,
+         EUR:0.91,
+         GBP:0.76,
+         INR:74.57,
+         PKR:280,
+
+    };
+
+let currency_converter  = await inquirer.prompt(
+    [
+        {
+            name:"from",
+            message:" FROM currency",
+            type:"list",
+            choices:["USD" ,"EUR","GBP","INR","PKR"]
+        },
+        {
+            name:"to",
+            message:" TO currency",
+            type:"list",
+            choices:["USD" ,"EUR","GBP","INR","PKR"]
+        },
+        {
+            name:"amount",
+            message:"Enter your amount",
+            type:"number"
+        }
+]
+)
+console.log(currency_converter)
+
+let fromAmount  = currency[currency_converter.from]
+
+let toAmount = currency[currency_converter.to]
+
+let amount = currency_converter.amount
+
+let  baseAmount = amount/fromAmount;
+
+let convertedAmount =baseAmount*toAmount;
+
+//console.log(fromAmount);
+//console.log(toAmount);
+//console.log(amount);
+//console.log(baseAmount);
+console.log(convertedAmount);
+
+ 
